310b6238688d3440420bf533287dd454792bd262
Felix Tang <tangyoutian@hotmail.com>
2019-04-03 15:22:31 +0800

    define destination index

--- script.py
+++ script.py
@@ -4,5 +4,9 @@ Los Angeles, USA
 So Paulo, Brazil
 Cairo, Egypt
 
-Destinations = [“Paris, France”, “Shanghai, China”, “Los Angeles, USA”, “São Paulo, Brazil”, “Cairo, Egypt”]
+destinations = [“Paris, France”, “Shanghai, China”, “Los Angeles, USA”, “São Paulo, Brazil”, “Cairo, Egypt”]
 test_traveler = ["Erin Wilkes", 'Shanghai, China', ['historical site', 'art']]
+def get_destination_index(destination):
+	destination_index = destination
+	return destination index
+
