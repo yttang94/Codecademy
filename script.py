2b887cf5dd6eda2fcce72398ed7375fd4ff54cb8
Felix Tang <tangyoutian@hotmail.com>
2019-04-03 15:36:17 +0800

    first function defined

--- script.py
+++ script.py
@@ -7,6 +7,7 @@ Cairo, Egypt
 destinations = [“Paris, France”, “Shanghai, China”, “Los Angeles, USA”, “São Paulo, Brazil”, “Cairo, Egypt”]
 test_traveler = ["Erin Wilkes", 'Shanghai, China', ['historical site', 'art']]
 def get_destination_index(destination):
-	destination_index = destination
-	return destination index
+	destination_index = destinations.index(destination)
+	return destination_index
 
+print (get_destination_index("Los Angeles, USA"))
