b22b35984ae933af4cd8229a0be5b69f97c068b3
Felix Tang <tangyoutian@hotmail.com>
2019-04-03 15:16:38 +0800

    Added test subjects

--- script.py
+++ script.py
@@ -3,3 +3,6 @@ Shanghai, China
 Los Angeles, USA
 So Paulo, Brazil
 Cairo, Egypt
+
+Destinations = [“Paris, France”, “Shanghai, China”, “Los Angeles, USA”, “São Paulo, Brazil”, “Cairo, Egypt”]
+test_traveler = ["Erin Wilkes", 'Shanghai, China', ['historical site', 'art']]
